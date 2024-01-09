d6717f119a993a7a970d154729cad3689bce8b5e
Glenn Chiang <glennchiang24@gmail.com>
2024-01-09 12:45:58 +0800

    Added function to resize and pad images However, output video is sometimes corrupted when given certain images

--- video_maker.py
+++ video_maker.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 import cv2
 import requests
 import numpy as np
@@ -8,29 +8,57 @@ def decode_img_from_url(image_url: str):
     res = requests.get(image_url)
     image_arr = np.asarray(bytearray(res.content), dtype=np.uint8)
     img = cv2.imdecode(image_arr, -1)
-    img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
     return img
 
 
-def create_video(images: List[np.ndarray]):
-    output_path = "output/video.mp4"
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    fps = 0.5
-    height, width, _ = images[0].shape
+def resize_and_pad_image(image: np.ndarray, window_size=Tuple[int, int]):
+    return cv2.resize(image, window_size)
+    win_height, win_width = window_size
+    img_height, img_width, channels = image.shape
 
-    writer = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
+    x_pos = (win_width - img_width) // 2  # top-left corner x
+    y_pos = (win_height - img_height) // 2  # top-left corner y
+
+    processed_image = np.zeros(
+        (win_height, win_width, channels), dtype=np.uint8)
+    processed_image[y_pos: y_pos + img_height,
+                    x_pos:x_pos + img_width, :] = image[:, :, :]
+    return processed_image
+
+
+def create_video(images: List[np.ndarray], video_size: Tuple[int, int], output_path):
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    fps = 0.25
+
+    writer = cv2.VideoWriter(output_path, fourcc, fps, video_size)
 
     for image in images:
         writer.write(image=image)
 
-    print('Done')
-
 
 def main():
-    image_urls = ['https://i.redd.it/3w7j7zit57bc1.jpeg', 'https://i.redd.it/emhtpd0nk1bc1.png',
-                  'https://i.redd.it/1i26k8vly6bc1.jpeg']
-    images = [decode_img_from_url(url) for url in image_urls]
-    create_video(images)
+    video_size = (800, 800)
+    image_urls = ['https://i.redd.it/hmx0q06oh8bc1.jpeg',
+                  'https://i.redd.it/ghqvhf063abc1.jpeg',
+                  'https://i.redd.it/btpa4n4ie7bc1.jpeg',
+                  'https://i.redd.it/3w7j7zit57bc1.jpeg',
+                  'https://i.redd.it/emhtpd0nk1bc1.png',
+                  'https://i.redd.it/1i26k8vly6bc1.jpeg',
+                  'https://i.redd.it/h41jkoe4wabc1.jpeg',
+                  'https://i.redd.it/itgb2tlcg8bc1.png',
+                  'https://i.redd.it/fe226bi827bc1.jpeg',
+                  'https://i.redd.it/s7a4g1rsj9bc1.png',
+                  'https://i.redd.it/5xrgtxys3bbc1.png']
+
+    original_images = [decode_img_from_url(url) for url in image_urls]
+    processed_images = [resize_and_pad_image(
+        img, window_size=video_size) for img in original_images]
+
+    # for idx, image in enumerate(processed_images):
+    #     # cv2.imwrite(f'output/image_{idx}.png', img=image)
+
+    create_video(processed_images, video_size, 'output/video.mp4')
+    print('Done')
 
 
 if __name__ == '__main__':
